cb8396a2e005bfc45dcbf51d6d6f7114c0d6abdc
chinhnguyen93 <ndchinh93@gmail.com>
2016-11-11 14:54:12 +0700

    kanban1
    123

--- Kanban/js/script.js
+++ Kanban/js/script.js
@@ -1,3 +1,27 @@
+
+var column_type = ['todo', 'doing', 'done'];
+
+var DB={
+	getData: function(){
+		if(typeof(Storage)!=="undefined"){
+			var data;
+			try{
+				data=JSON.parse(localStorage.getItem("list"))||{};
+			} catch(error){
+				data={};
+			}
+			return data;
+		} else{
+    	alert("K support");
+    	return {};
+		}
+	},
+	setData: function(data){
+		localStorage.setItem("list", JSON.stringify(data));
+	}
+};
+
+var list = DB.getData();
 var app={
 	newJob: function(even, type, input){
 		var jobName=$(input).val();
@@ -5,7 +29,10 @@ var app={
 		if(event.keyCode==13){
 			if(jobName.trim()!=''){
 				//Update DOM
-				this.addJobToList(type, jobName)
+				if(!list[type]) list[type]=[];
+				list[type].push(jobName);
+				DB.setData(list);
+				this.addJobToList(type, jobName);
 				//Reset input
 				$(input).val('');
 			}
@@ -16,7 +43,6 @@ var app={
 		$('#'+type).append(item);
 	},
 	deleteJobToList: function(span){
-			console.log("abc");
 			$('.modal').css('display','block');
 			$('#btn-delete').on('click',function(){				
 				$('.modal').css('display','none');
@@ -31,6 +57,12 @@ var app={
 
 
 $( function() {
+			column_type.forEach(function(type){
+				var columntype = list[type] || [];
+				columntype.forEach(function(jobName){
+					app.addJobToList(type, jobName);
+				});
+			})
 	    $('.sorted-list').sortable({
 	      connectWith: '.sorted-list',
 	      placeholder: 'ui-state-highlight'
